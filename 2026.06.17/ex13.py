6496020b38918ec346f994e6842a6664271028be
André Luferat <contato@luferat.net>
2026-06-17 16:50:17 -0300

    Update ex13.py

--- 2026.06.17/ex13.py
+++ 2026.06.17/ex13.py
@@ -23,3 +23,32 @@ Desafio extra
 
  - Permita até três tentativas.
 '''
+
+datauser = [
+    "admin", "1234"
+]
+
+is_autorized = False
+
+triers = 1
+while True:
+    print("\n------------------------------")
+    username = input("Usuário: ")
+    password = input("Senha: ")
+    if username == datauser[0] and password == datauser[1]:
+        is_autorized = True
+        break
+
+    if triers == 3:
+        print("Tentativas esgotadas")
+        break
+
+    triers += 1
+    print("Dados inválidos. Tente novamente.")
+
+
+if is_autorized:
+    print("Acesso permitido")
+else:    
+    print("Acesso negado")
+
