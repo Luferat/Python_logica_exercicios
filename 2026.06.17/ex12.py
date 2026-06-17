de1e485acb696e6a6de9f2dcebd6cee7ff2f393d
André Luferat <contato@luferat.net>
2026-06-17 16:28:20 -0300

    Update ex12.py

--- 2026.06.17/ex12.py
+++ 2026.06.17/ex12.py
@@ -29,3 +29,25 @@ Desafio extra
 
  - Calcule o valor total de todos os produtos.
 '''
+
+# Lista de dicionários
+products = [
+    {
+        "name": "Mouse",
+        "price": 50
+    },
+    {
+        "name": "Keyboard",
+        "price": 120.99
+    }
+]
+
+print("\n")
+
+total = 0
+
+for product in products:
+    total += product['price']
+    print(f"{product['name']} - R${product['price']}")
+
+print(f"\nTotal de R${total}")
