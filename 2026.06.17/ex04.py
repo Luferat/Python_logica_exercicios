c0db5f18cc45e8567b414b0c46c894e46568f871
André Luferat <contato@luferat.net>
2026-06-17 14:54:02 -0300

    Update ex04.py

--- 2026.06.17/ex04.py
+++ 2026.06.17/ex04.py
@@ -15,3 +15,21 @@ Desafio extra
 
  - Informe também se o número é par ou ímpar.
 '''
+
+print('\n')
+num = int(input("Digite um numero inteiro: "))
+
+# Positivo, negativo ou zero
+if num > 0:
+    print(f'{num} é positivo...')
+elif num < 0:
+    print(f'{num} é negativo...')
+else:
+    print("É zero...")
+
+
+# Par ou ímpar
+if num % 2 == 0:
+    print("... e é par")
+else:
+    print("... e é ímpar")
