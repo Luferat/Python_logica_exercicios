b3b6e0532e64cc0add2aab06329218c8f57dacd1
André Luferat <contato@luferat.net>
2026-06-22 16:13:23 -0300

    Update ex02.py

--- 2026.06.22/ex02.py
+++ 2026.06.22/ex02.py
@@ -16,3 +16,25 @@ Notebook - R$ 3500.00
 Desafio
 Crie três produtos diferentes e exiba todos.
 '''
+
+
+class Product:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def show_info(self):
+        print(f'{self.name} - R$ {self.price}')
+
+
+print("\n")
+
+prods = [
+    Product("Notebook", 3500.00),
+    Product("Caneta", 250.99),
+    Product("Impressora", 800.99),
+]
+
+for prod in prods:
+    prod.show_info()
