238d389687555b1855d747ebcdd49c9528216e72
André Luferat <contato@luferat.net>
2026-06-22 16:27:22 -0300

    Update ex03.py

--- 2026.06.22/ex03.py
+++ 2026.06.22/ex03.py
@@ -17,3 +17,29 @@ Desafio
 Evite divisão por zero.
 '''
 
+
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            return "ERRO: divisão por 0"
+        return a / b
+
+
+calc = Calculator()
+
+print("\n")
+print("Soma: ", calc.add(10, 5))
+print("Subtração: ", calc.subtract(10, 5))
+print("Multiplicação: ", calc.multiply(10, 5))
+print("Divisão: ", calc.divide(10, 5))
+print("Divisão: ", calc.divide(5, 0))
