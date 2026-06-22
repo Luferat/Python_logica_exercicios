8c5b7e6e46a0affb28b559632343bdf58aa8141d
André Luferat <contato@luferat.net>
2026-06-22 16:07:17 -0300

    Update ex01.py

--- 2026.06.22/ex01.py
+++ 2026.06.22/ex01.py
@@ -12,3 +12,15 @@ Age: 25
 Dica
  - Atributos podem ser criados dentro do método __init__().
 '''
+
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("João", 25)
+
+print('\n')
+print(f'Name: {p1.name}')
+print(f'Age: {p1.age}')
